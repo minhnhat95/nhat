dbd4c16fc4f94a6beffbe844355ce192ac863581
minhnhat95 <30515340+minhnhat95@users.noreply.github.com>
2017-07-28 13:54:22 +0700

    try

--- wowza.php
+++ wowza.php
@@ -3,24 +3,24 @@
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL, "https://api.cloud.wowza.com/api/v1/live_streams/");
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://api-sandbox.cloud.wowza.com/api/v1/live_streams?page=https%3A%2F%2Fapi.cloud.wowza.com%2Fapi%2Fv1%2Flive_streams%2F");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 
 
 $headers = array();
-$headers[] = "wsc-api-key: zrlWvVapoRIG50b6TufAqBrhBpjCGQhQsT4ttgiYE5gwKWhQgaswZGWtGFKL3630";
-$headers[] = "wsc-access-key: pMSDulbhMeDND9QzzIHyQIBOuAJKUufgqFQ8xcjVIQUvM7DDGXTEJjzZGKzp3454";
-echo $headers[0];
-echo $headers[1];
+$headers[] = "Accept: application/json";
+$headers[] = "Wsc-Api-Key: zrlWvVapoRIG50b6TufAqBrhBpjCGQhQsT4ttgiYE5gwKWhQgaswZGWtGFKL3630";
+$headers[] = "Wsc-Access-Key: pMSDulbhMeDND9QzzIHyQIBOuAJKUufgqFQ8xcjVIQUvM7DDGXTEJjzZGKzp3454";
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
 $result = curl_exec($ch);
 if (curl_errno($ch)) {
     echo 'Error:' . curl_error($ch);
 }
-else
-	echo $result;
+else echo $result;
 curl_close ($ch);
 
 ?>
