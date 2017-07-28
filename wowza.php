b7f629f9307f2f3192bdee8f120689f22a58d17f
minhnhat95 <30515340+minhnhat95@users.noreply.github.com>
2017-07-28 13:21:30 +0700

    fix

--- wowza.php
+++ wowza.php
@@ -1,22 +1,24 @@
 
 <?php
 
-	echo "hello";
+	echo "hello\n";
 	$ch = curl_init();
 
 	curl_setopt($ch, CURLOPT_URL, "https://api.cloud.wowza.com/api/v1/live_streams");
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
 
-
+	echo "1\n";
+	
 	$headers = array();
 	$headers[] = "Wsc-Api-Key: zrlWvVapoRIG50b6TufAqBrhBpjCGQhQsT4ttgiYE5gwKWhQgaswZGWtGFKL3630";
 	$headers[] = "Wsc-Access-Key: pMSDulbhMeDND9QzzIHyQIBOuAJKUufgqFQ8xcjVIQUvM7DDGXTEJjzZGKzp3454";
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
+	echo "2\n";
 	$result = curl_exec($ch);
+	echo "3\n";
 	if (curl_errno($ch)) {
-		echo "error";
+		echo "error\n";
 		echo 'Error:' . curl_error($ch);
 	}
 	else 
